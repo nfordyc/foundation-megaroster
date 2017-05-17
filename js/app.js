79f79a855fe59ba451cdd632b8af910a7cf441d1
Nicholas Fordyce <FordyceNick@gmail.com>
2017-05-17 14:46:17 -0400

    Set up submit handler for new student form

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
 $(document).foundation()
+
+const megaroster = {
+    init() {
+        document
+            .querySelector('#new-student')
+            .addEventListener('submit', this.addStudents);
+    },
+
+    addStudents(e) {
+        e.preventDefault()
+        const studentName = e.target.studentName.value
+        console.log(studentName)
+    },
+}
+megaroster.init()
