1dac2563db25aa4384369c4e07c4f32a6590d4ad
Nicholas Fordyce <FordyceNick@gmail.com>
2017-05-17 15:10:44 -0400

    Create student object with a name and id

--- js/app.js
+++ js/app.js
@@ -2,15 +2,25 @@ $(document).foundation()
 
 const megaroster = {
     init() {
+        this.max = 0
         document
             .querySelector('#new-student')
-            .addEventListener('submit', this.addStudents);
+            .addEventListener('submit', this.addStudents.bind(this));
     },
 
     addStudents(e) {
         e.preventDefault()
-        const studentName = e.target.studentName.value
-        console.log(studentName)
+        const f = e.target
+        const student = {
+            id: this.max + 1,
+            name: f.studentName.value,
+        }
+        this.max++
+        this.buildListItem(student)
+    },
+
+    buildListItem(student){
+        console.log(student)
     },
 }
 megaroster.init()
